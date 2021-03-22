9670ca3036e18277f92a399a554b8d4532a58fe2
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-22 22:25:31 +0700

    Add channel in sidebar

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import AddIcon from '@material-ui/icons/Add';
 import { sidebarItems } from '../../Data/SideBarData';
+import db from '../../firebase';
+// import Modal from 'react-modal';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+	console.log(props);
+	// const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const addChannel = () => {
+		const promptName = prompt('Input your New Channel');
+		if (promptName) {
+			db.collection('rooms').add({
+				name: promptName,
+			});
+		}
+	};
+
 	return (
 		<div className='sidebar__container'>
 			<div className='sidebar__container__workspace'>
@@ -23,13 +37,23 @@ const Sidebar = () => {
 			<div className='sidebar__channel'>
 				<div className='sidebar__channel--new'>
 					<div>Channels</div>
-					<AddIcon />
+					<AddIcon onClick={addChannel} />
+					{/* <AddIcon onClick={() => setIsModalOpen(true)} /> */}
 				</div>
 				<div className='sidebar__channel--list'>
-					<div className='sidebar__channel--list-name'># Channel 1</div>
-					<div className='sidebar__channel--list-name'># Channel 2</div>
+					{props.rooms.map((item) => (
+						<div className='sidebar__channel--list-name'># {item.name}</div>
+					))}
 				</div>
 			</div>
+			{/* <Modal
+				isOpen={isModalOpen}
+				onRequestClose={() => {
+					setIsModalOpen(false);
+				}}
+				className='modal-container'
+				overlayClassName='modal-overlay-center'
+				contentLabel='Add Channel'></Modal> */}
 		</div>
 	);
 };
