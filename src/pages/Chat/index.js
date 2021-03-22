b7cf306b3cec0fb4e7e2bf70b3b0ffb963bb3fd3
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-22 22:24:51 +0700

    [Page] - Add new room on chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../Components/Header';
 import Sidebar from '../../Components/SideBar';
 import Body from '../../Components/Body';
 import Footer from '../../Components/Footer';
+import db from '../../firebase';
 
-const Chat = () => {
+const Chat = (props) => {
+	const [rooms, setRooms] = useState([]);
+
+	const getChannels = () => {
+		db.collection('rooms').onSnapshot((snapshot) => {
+			setRooms(
+				snapshot.docs.map((doc) => {
+					return { id: doc.id, name: doc.data().name };
+				})
+			);
+		});
+	};
+
+	useEffect(() => {
+		getChannels();
+	}, []);
+
+	// console.log(rooms);
+
 	return (
 		<div>
-			<Header />
+			<Header header={props.user} />
 			<div className='chat__body__container'>
-				<Sidebar />
+				<Sidebar rooms={rooms} />
 				<Body />
 			</div>
 			<Footer />
