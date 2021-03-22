56f32c6d9375ce013d8de06d4a10ad6e0a8b9b4f
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-22 22:27:05 +0700

    Add login function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 
 function App() {
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+	console.log('user in app state', user);
 	return (
 		<div>
-			<BrowserRouter>
-				<Switch>
-					<Route path='/room' component={Chat} />
-					<Route exact path='/' component={Login} />
-				</Switch>
-			</BrowserRouter>
+			<Router>
+				{!user ? (
+					<Login setUser={setUser} />
+				) : (
+					<Switch>
+						<Route path='/room'>
+							<Chat user={user} />
+						</Route>
+					</Switch>
+				)}
+			</Router>
 		</div>
 	);
 }
