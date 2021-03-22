f883a0884f55de4ef1b951443c21f26f7c5decc8
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-22 22:23:04 +0700

    [Components] - Add chat input and style

--- src/Components/Body/index.js
+++ src/Components/Body/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import ChatInput from '../ChatInput';
+import ChatMessage from '../ChatMessage';
 
 const Body = () => {
 	return (
 		<div className='body__container'>
-			<h1>It's Body</h1>
+			<div className='body__header'>
+				<div className='body__header__channel'>
+					<div className='body__header__channel--name'># Channel </div>
+					<div className='body__header__channel--info'>Company-wide announcements and work-based matters</div>
+				</div>
+				<div className='body__header__channelDetails'>
+					<div>Details</div>
+					<InfoOutlinedIcon />
+				</div>
+			</div>
+			<div className='body__message'>
+				<ChatMessage />
+			</div>
+			<ChatInput />
 		</div>
 	);
 };
