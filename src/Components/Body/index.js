7b5aa00917e8c74c92c04c9935c8d61574497148
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-26 17:16:22 +0700

    Realtime chat function

--- src/Components/Body/index.js
+++ src/Components/Body/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import ChatInput from '../ChatInput';
 import ChatMessage from '../ChatMessage';
+import db from '../../firebase';
+import { useParams } from 'react-router-dom';
+import firebase from 'firebase';
 
-const Body = () => {
+const Body = ({ body }) => {
+	let { channelId } = useParams();
+	const [channel, setChannel] = useState();
+	const [messages, setMessages] = useState([]);
+
+	const getMessages = () => {
+		db.collection('rooms')
+			.doc(channelId)
+			.collection('messages')
+			.orderBy('timestamp', 'asc')
+			.onSnapshot((snapshot) => {
+				let messages = snapshot.docs.map((doc) => doc.data());
+				console.log('message =>', messages);
+				setMessages(messages);
+			});
+	};
+
+	const sendMessage = (text) => {
+		if (channelId) {
+			let payload = {
+				text: text,
+				user: body.name,
+				userImage: body.photo,
+				timestamp: firebase.firestore.Timestamp.now(),
+			};
+			db.collection('rooms').doc(channelId).collection('messages').add(payload);
+			console.log('payload =>', payload);
+		}
+	};
+
+	const getChannel = () => {
+		db.collection('rooms')
+			.doc(channelId)
+			.onSnapshot((snapshot) => {
+				setChannel(snapshot.data()); // snapshot how to we get data, the data of that channel, onSnapshot means whenever the snapshot changes, it'll modify something / called again
+				// console.log('Snapshot =>', snapshot.data());
+			});
+	};
+
+	useEffect(() => {
+		getChannel();
+		getMessages();
+	}, [channelId]);
+
 	return (
 		<div className='body__container'>
 			<div className='body__header'>
 				<div className='body__header__channel'>
-					<div className='body__header__channel--name'># Channel </div>
+					<div className='body__header__channel--name'># {channel && channel.name} </div>
 					<div className='body__header__channel--info'>Company-wide announcements and work-based matters</div>
 				</div>
 				<div className='body__header__channelDetails'>
@@ -17,9 +63,9 @@ const Body = () => {
 				</div>
 			</div>
 			<div className='body__message'>
-				<ChatMessage />
+				{messages.length > 0 && messages.map((data, index) => <ChatMessage text={data.text} name={data.user} image={data.userImage} timestamp={data.timestamp} />)}
 			</div>
-			<ChatInput />
+			<ChatInput sendMessage={sendMessage} />
 		</div>
 	);
 };
--- src/Components/ChatInput/index.js
+++ src/Components/ChatInput/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SendIcon from '@material-ui/icons/Send';
 
-const ChatInput = () => {
+const ChatInput = ({ sendMessage }) => {
+	const [input, setInput] = useState('');
+
+	const send = (event) => {
+		event.preventDefault();
+		if (!input) return;
+		sendMessage(input);
+		setInput('');
+	};
 	return (
 		<div className='chatInput__container'>
 			<div className='chatInput__input'>
 				<form>
-					<input type='text' placeholder='Message here...' />
+					<input onChange={(event) => setInput(event.target.value)} value={input} type='text' placeholder='Message here...' />
 					<div className='chatInput__button'>
-						<SendIcon />
+						<SendIcon type='submit' onClick={send} />
 					</div>
 				</form>
 			</div>
