95233508447c7745a87869230de9fc7ef703b928
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-26 17:15:54 +0700

    Add dropdown for logout

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const Header = ({ header }) => {
+const Header = (props) => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggle = () => setDropdownOpen((prevState) => !prevState);
+
 	const signOut = () => {
 		localStorage.removeItem('user');
 		window.location.reload();
 	};
+
 	return (
 		<div className='header__container'>
-			<div>toggle</div>
 			<div className='header__main'>
 				<AccessAlarmIcon />
 				<div className='header__search'>
@@ -20,9 +25,16 @@ const Header = ({ header }) => {
 				<HelpOutlineIcon />
 			</div>
 			<div className='header__user--container'>
-				<div className='header__user--name'>{header.name}</div>
-				<div onClick={signOut} className='header__user--image'>
-					<img src={header.photo ? header.photo : 'https://i.imgur.com/6VBx3io.png'} alt='profile-picture' />
+				<div className='header__user--name'>{props.user && props.user.name}</div>
+				<div className='header__user--image'>
+					<Dropdown isOpen={dropdownOpen} toggle={toggle}>
+						<DropdownToggle style={{ border: 'none', backgroundColor: 'transparent' }}>
+							<img className='img-dropdown' src={props.user && props.user.photo ? props.user.photo : 'https://i.imgur.com/6VBx3io.png'} alt='profile-picture' />
+						</DropdownToggle>
+						<DropdownMenu right>
+							<DropdownItem onClick={signOut}>Log Out</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
 				</div>
 			</div>
 		</div>
