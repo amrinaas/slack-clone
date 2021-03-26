96e3a886e676bbbe377f500626aa6e77bec23705
Amrina Aulia Siregar <amrinaas96@gmail.com>
2021-03-26 17:16:35 +0700

    Add new assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
+import Header from './Components/Header';
+import Sidebar from './Components/SideBar';
+import Select from './Components/SelectChannel';
+import db from './firebase';
 
 function App() {
 	const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
-	console.log('user in app state', user);
+	const [rooms, setRooms] = useState([]);
+
+	const getChannels = () => {
+		db.collection('rooms').onSnapshot((snapshot) => {
+			setRooms(
+				snapshot.docs.map((doc) => {
+					return { id: doc.id, name: doc.data().name };
+				})
+			);
+		});
+	};
+
+	useEffect(() => {
+		getChannels();
+	}, []);
+
+	const signOut = () => {
+		localStorage.removeItem('user');
+		window.location.reload();
+	};
+
+	// console.log('user in app state', user);
 	return (
 		<div>
 			<Router>
 				{!user ? (
 					<Login setUser={setUser} />
 				) : (
-					<Switch>
-						<Route path='/room'>
-							<Chat user={user} />
-						</Route>
-					</Switch>
+					<div className='app-container'>
+						<Header signOut={signOut} user={user} />
+						<div className='app-main'>
+							<Sidebar rooms={rooms} user={user} />
+							<Switch>
+								<Route path='/room/:channelId'>
+									<Chat user={user} />
+								</Route>
+								<Route path='/'>
+									<Select />
+								</Route>
+							</Switch>
+						</div>
+					</div>
 				)}
 			</Router>
 		</div>
